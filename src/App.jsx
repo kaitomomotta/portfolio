933e1611cd72c50b5efe4d2bf0e534cd137b3189
abel.roinsard <abel.roinsard@epita.fr>
2024-07-04 18:49:01 +0200

    test router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { NextUIProvider } from "@nextui-org/react"
+import React from "react"
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const navigate = useNavigate();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <NextUIProvider navigate={navigate}>
+      <Routes>
+        <Route path="/portfolio" element={<h1>Hello World</h1>}/>
+        <Route path="/portfolio/me" element={<h1>Me</h1>}/>
+      </Routes>
+      <h1 className="text-3xl font-bold underline">
+        main website
+      </h1>
+    </NextUIProvider>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { NextUIProvider } from '@nextui-org/react'
+import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <main className='full-window dark text-foreground bg-background'>
+        <App />
+      </main>
+    </BrowserRouter>
   </React.StrictMode>,
 )
