cc8829ef1ea6229d529dcd3f509818f091e9ca93
Abel Roinsard <abelroinsard@gmail.com>
2024-11-25 23:27:19 +0100

    columns layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="columns">
-        <div className="column">First column</div>
+        <div className="column is-half">First column</div>
         <div className="column">Second column</div>
         <div className="column">Third column</div>
-        <div className="column">Fourth column</div>
       </div>
     </>
   );
