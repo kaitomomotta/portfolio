dc2ca4fc7347c9e3cad1c9d5d22eac5a361870ab
Abel Roinsard <abelroinsard@gmail.com>
2024-11-27 00:09:34 +0100

    remove bulma, find mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import "./App.css";
 import {styled} from "@mui/system";
+import Columns from "./components/Columns.tsx";
+import {useEffect, useState} from "react";
 
 const UpBox = styled('div')({
     paddingTop: '12rem'
 })
 
 function App() {
+    const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <>
             <UpBox/>
-            <div className="columns">
-                <div className="column is-half">First column</div>
-                <div className="column">Second column</div>
-                <div className="column">Third column</div>
-            </div>
+            <Columns isMobile={isMobile}/>
         </>
     );
 }
