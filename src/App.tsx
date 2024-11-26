76637cd51cfb3d69d5ffd6dd3a726956f3c701cd
Abel Roinsard <abelroinsard@gmail.com>
2024-11-26 22:56:50 +0100

    install mui styled, fix columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import "./App.css";
+import {styled} from "@mui/system";
+
+const UpBox = styled('div')({
+    paddingTop: '12rem'
+})
 
 function App() {
-  return (
-    <>
-      <div className="columns">
-        <div className="column is-half">First column</div>
-        <div className="column">Second column</div>
-        <div className="column">Third column</div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <UpBox/>
+            <div className="columns">
+                <div className="column is-half">First column</div>
+                <div className="column">Second column</div>
+                <div className="column">Third column</div>
+            </div>
+        </>
+    );
 }
 
 export default App;
