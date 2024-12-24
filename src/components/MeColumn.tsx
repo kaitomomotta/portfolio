4ffb19e5d13c7bdc61fbeb3ccd54be9ef7fe7055
Abel Roinsard <abelroinsard@gmail.com>
2024-12-24 07:24:52 +0100

    headers

--- src/components/MeColumn.tsx
+++ src/components/MeColumn.tsx
@@ -1,13 +1,32 @@
 import {Box, styled} from "@mui/system";
 
-const Title = styled(Box)(({ theme }) => ({
+const Text = styled(Box)(({ theme }) => ({
   color: theme.palette.secondary.accent2Dark,
+}));
+
+const Title = styled(Text)({
+  fontSize: '128px',
+  fontWeight: '800'
+});
+
+const Subtitle = styled(Text)({
+  fontSize: '24px',
+  fontWeight: '800'
+});
+
+const Link = styled('a')(({ theme }) => ({
+  color: theme.palette.secondary.accent2Dark,
+  fontWeight: '600'
 }));
 
 export default function MeColumn() {
   return(
       <>
-        <Title>aaa</Title>
+        <Title>abel</Title>
+        <Subtitle>computer science student</Subtitle>
+        <Text>Epita 4th student - MTI major</Text>
+        <Text>芝浦工業大学 exchange student</Text>
+        <Link href='/portfolio/cv.pdf'>Download my CV {'>'}</Link>
       </>
   );
 }
